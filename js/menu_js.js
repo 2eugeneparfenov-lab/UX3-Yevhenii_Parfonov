d57bd1fb0b868b8951479403fcf8d62aea7a8e34
Yevhenii Parfonov <yevhenii.parfonov@student.tuke.sk>
2025-12-15 03:31:17 +0100

    Add new pages

--- js/menu_js.js
+++ js/menu_js.js
@@ -1,5 +1,15 @@
-const card = document.querySelector(".menu_rect");
+document.getElementById("profile").addEventListener("click", () => {
+    window.location.href = "profile.html";
+});
 
-card.addEventListener('click', () => {
-    window.location.href = 'profile.html';
+document.getElementById("practice").addEventListener("click", () => {
+    window.location.href = "practice.html";
+});
+
+document.getElementById("focus").addEventListener("click", () => {
+   window.location.href = "focus.html";
+});
+
+document.getElementById("chat").addEventListener("click", () => {
+    window.location.href = "chat_bot.html";
 });
