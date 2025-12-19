03667342c6345a25ce954d97216b4b4fba1012a0
Yevhenii Parfonov <yevhenii.parfonov@student.tuke.sk>
2025-12-19 03:00:19 +0100

    Add total redesign v1.3

--- js/index.js
+++ js/index.js
@@ -69,11 +69,51 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target === Modal4) Modal4.classList.remove('show');
     });
 
+    // ===== MODAL5 & MODAL6 =====
+    const modal5 = document.getElementById('modal5');
+    const closeModal5 = document.getElementById('closeConfirmMail');
+    const continueModal5 = modal5.querySelector('.login-btn'); // кнопка Continue у modal5
+    const modal6 = document.getElementById('modal6');
+    const closeModal6 = document.getElementById('closeConfirmMailConf');
+
+    closeModal5.addEventListener('click', () => modal5.classList.remove('show'));
+    modal5.addEventListener('click', e => { if (e.target === modal5) modal5.classList.remove('show'); });
+
+    continueModal5.addEventListener('click', () => {
+        modal5.classList.remove('show');
+        modal6.classList.add('show');
+    });
+
+    closeModal6.addEventListener('click', () => modal6.classList.remove('show'));
+    modal6.addEventListener('click', e => { if (e.target === modal6) modal6.classList.remove('show'); });
+
+    // ===== OPEN MODAL5 FROM "Forgot password?" =====
+    const forgotPassword = document.querySelector('.forgot-password a');
+
+    forgotPassword.addEventListener('click', (e) => {
+        e.preventDefault(); // щоб не було переходу за посиланням
+        loginModal.classList.remove('show'); // ховаємо логін, якщо відкритий
+        modal5.classList.add('show');        // показуємо модал5
+    });
+
 });
 
 function goMainMenu() {
     window.location.href = 'pages/menu_page.html';
 }
+
+function goMainMenuJack() {
+    const usernameInput = document.querySelector('#modal input[type="text"]').value;
+    const passwordInput = document.querySelector('#modal input[type="password"]').value;
+
+    if (usernameInput === 'Jack' && passwordInput === '123') {
+        // правильний логін
+        window.location.href = 'pages/menu_page.html';
+    } else {
+        alert('Incorrect username or password!');
+    }
+}
+
 
 
 
